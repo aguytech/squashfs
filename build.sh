2db86e417399165aa51d1d25858cd097521d7503
aguy tech <aguytech@free.fr>
2022-06-24 19:02:40 +0200

    add builder

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+! hash apt 2>/dev/null && echo "Unable to find apt" && exit 1
+
+echo "> install required packages"
+echo
+sudo apt install -y build-essential liblzma-dev liblzo2-dev zlib1g-dev
+
+cd $(dirname $(readlink -f $0))
+
+CFLAGS=-fcommon make || exit 1
+sudo make install || exit 1
+
+echo
+echo "> Installation succeded"

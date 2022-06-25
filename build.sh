66bd3ebee057e237657b47d2b34d3965f838f181
aguy tech <aguytech@free.fr>
2022-06-25 09:46:06 +0200

    update builder

--- build.sh
+++ build.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-! hash apt 2>/dev/null && echo "Unable to find apt" && exit 1
+! hash apt 2>/dev/null && echo "This installation is for Ubuntu OS" && exit 1
 
-echo "> install required packages"
+echo "> installs required packages"
 echo
-sudo apt install -y build-essential liblzma-dev liblzo2-dev zlib1g-dev
+sudo apt install -y build-essential liblzma-dev liblzo2-dev zlib1g-dev || { echo; echo "Unable to continue without packages installed"; exit 1; }
 
-cd $(dirname $(readlink -f $0))/squashfs
+path=$( dirname $( readlink -f $0 ) )/squashfs4.3
+cd ${path} || { echo; echo "Unable find path ${path}"; exit 1; }
 
 CFLAGS=-fcommon make || exit 1
 sudo make install || exit 1
 
 echo
 echo "> Installation succeded"
+

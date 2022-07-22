3a3e039b7efbf4c7f7c4056fdc4f966769dd002d
aguy tech <aguytech@free.fr>
2022-07-22 13:54:59 +0200

    Revert to 66bd3ebee05

--- build.sh
+++ build.sh
@@ -2,11 +2,6 @@
 
 ! hash apt 2>/dev/null && echo "This installation is for Ubuntu OS" && exit 1
 
-echo "> Create temporary folder"
-path=/tmp/squashfs.$(date +%s)
-mkdir ${path} || { echo "Unable to create path; ${path}" && exit 1; }
-cd ${path}
-
 echo "> installs required packages"
 echo
 sudo apt install -y build-essential liblzma-dev liblzo2-dev zlib1g-dev || { echo; echo "Unable to continue without packages installed"; exit 1; }
@@ -19,4 +14,3 @@ sudo make install || exit 1
 
 echo
 echo "> Installation succeded"
-
